c239f4788d0bc84a439f1fdf1345341bb78dcc4f
Camilo Gomez <camilogomezmusic@gmail.com>
2020-08-11 22:07:51 +0000

    Added ajax request to get tweets from server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,32 +6,6 @@
 
 
 
- // Temporarily import the "database"
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-
 $(document).ready( function() {
   
   const createTweetElement = function(tweetData) {
@@ -62,7 +36,6 @@ $(document).ready( function() {
     }
   };
 
-  renderTweets(data);
 
   $('#submit-tweet').submit(function(event) {
     event.preventDefault();
@@ -71,7 +44,13 @@ $(document).ready( function() {
     .then(function(tweet) {
       console.log('Success ');
     });
-  })
+  });
+
+  const loadTweets = function() {
+    $.ajax('/tweets', {method: 'GET'}).then(renderTweets);
+  }
+  loadTweets();
+
 });
 
 

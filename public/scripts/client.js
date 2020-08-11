289f3546b9879faef45525409ffc75333ec61e28
Camilo Gomez <camilogomezmusic@gmail.com>
2020-08-11 20:35:57 +0000

    added JS to dynamically create tweets. Used a hard coded database for now. Added moment.js in order to show how long ago the tweet was created

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,70 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+ // Temporarily import the "database"
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+
+$(document).ready( function() {
+  const createTweetElement = function(tweetData) {
+    const $tweet = `
+    <article class="tweet">
+      <header>
+        <div class="username">
+          <div class="avatar"><img src="${tweetData.user.avatars}" height= 50px; ></div>
+          <div class="name">${tweetData.user.name}</div>
+        </div>
+        <div class="handle">${tweetData.user.handle}</div>
+      </header>
+      <div class="tweet-content">${tweetData.content.text}</div>
+      <footer>
+        <div class="timestamp">${moment(tweetData.created_at).fromNow()}</div>
+        <div class="icons"> 
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+    </article> `;
+
+    return $tweet;
+  };
+  
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      $('#tweet-container').append(createTweetElement(tweet));
+    }
+  };
+
+  renderTweets(data);
+
+});
+
+
+
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,13 +2,12 @@ $(document).ready(function() {
   
   $('#tweet-text').on('keyup', function() {
     const tweetLength = 140 - $(this).val().length;
-    const counter = $(this).parents().find('.counter');
-    counter.val(tweetLength)
+    const counter = $(this).parent().find('.counter');
+    counter.val(tweetLength);
 
     if (counter.val() < 0)
       counter.addClass('red');
     else
       counter.removeClass('red');
-  })
-
+  });
 });
